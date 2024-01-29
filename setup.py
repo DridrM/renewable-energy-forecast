cd0fbcc9e8c0e51f14997716c17f125c3cefb316
Hadrien Moncomble <hmoncomble@gmail.com>
2024-01-29 21:23:40 +0100

    Test API call completed

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from setuptools import find_packages
+
+# List the dependencies frome the requirements.txt
+with open("requirements.txt") as r:
+    # Read the content
+    content = r.readlines()
+
+# Store the requirements into a list
+requirements = [l.strip() for l in content]
+
+# Setup our package
+setup(name = "re_forecast",
+      version = "0.0",
+      description = "Predict renewable energy production",
+      packages = find_packages(),
+      install_requires = requirements)
