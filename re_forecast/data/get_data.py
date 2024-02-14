1aa955730bf07a396361eba701ad75c6e984b216
Hadrien Moncomble <hmoncomble@gmail.com>
2024-02-14 22:58:34 +0100

    Started get_data functions

---
+++ re_forecast/data/get_data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+from re_forecast.data.load_data import download_rte_data
+from re_forecast.data.format_data import extract_generation_units, extract_generation_values, extract_all_generation_values
+from re_forecast.data.store_data import store_to_csv
+from re_forecast.params import DATA_CSV_ENERGY_PRODUCTION_PATH
+
+
+def get_rte_data() -> pd.DataFrame:
+    """"""
+
+
+def get_rte_units_names() -> pd.DataFrame:
+    """"""
